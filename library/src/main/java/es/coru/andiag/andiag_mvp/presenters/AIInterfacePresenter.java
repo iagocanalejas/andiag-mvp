5a6143949b2f5cef344453c04ca61d4902daf259
Iago Santos Dominguez <iagocanalejas@gmail.com>
2016-12-25 16:46:22 +0100

    Added documentation for presenters

--- library/src/main/java/es/coru/andiag/andiag_mvp/presenters/AIInterfacePresenter.java
+++ library/src/main/java/es/coru/andiag/andiag_mvp/presenters/AIInterfacePresenter.java
@@ -1,21 +1,46 @@
 package es.coru.andiag.andiag_mvp.presenters;
 
 /**
- * Created by Iago on 17/12/2016.
+ * Created by Canalejas on 17/12/2016.
  */
-
 public interface AIInterfacePresenter<C, V> {
 
+    /**
+     * @return presenter's view or null
+     */
     V getView();
 
+    /**
+     * @return presenter's context or null
+     */
     C getContext();
 
+    /**
+     * Add context and view to the presenter
+     *
+     * @param context new context {@link android.app.Application} {@link android.app.Activity} ...
+     * @param view    new view {@link android.app.Activity} {@link android.app.Fragment} ...
+     */
     void attach(C context, V view);
 
+    /**
+     * Remove view and context if exist from presenter
+     */
     void detach();
 
+    /**
+     * @return true if view is attached to presenter false otherwise
+     */
     boolean isViewAttached();
 
+    /**
+     * @return true if view is showing false otherwise
+     */
+    boolean isViewCreated();
+
+    /**
+     * @return true if context exist false otherwise
+     */
     boolean hasContext();
 
 }
--- library/src/main/java/es/coru/andiag/andiag_mvp/presenters/AIPresenter.java
+++ library/src/main/java/es/coru/andiag/andiag_mvp/presenters/AIPresenter.java
@@ -3,47 +3,77 @@ package es.coru.andiag.andiag_mvp.presenters;
 import android.support.annotation.NonNull;
 
 /**
- * Created by Iago on 17/12/2016.
+ * Created by Canalejas on 17/12/2016.
+ * All presenters must extends this class or implements {@link AIInterfacePresenter}
  */
-
 public abstract class AIPresenter<C, V> implements AIInterfacePresenter<C, V> {
 
     private V mView;
     private C mContext;
+
+    private boolean isViewAttached = false;
     private boolean isViewCreated = false;
 
     protected AIPresenter() {
     }
 
+    /**
+     * {@link AIInterfacePresenter#attach}
+     */
     @Override
     public final void attach(C context, @NonNull V view) {
+        this.isViewAttached = true;
         this.mView = view;
         this.mContext = context;
         onViewAttached();
     }
 
+    /**
+     * {@link AIInterfacePresenter#detach}
+     */
     @Override
     public final void detach() {
+        this.isViewAttached = false;
         this.mView = null;
         this.mContext = null;
         onViewDetached();
     }
 
+    /**
+     * {@link AIInterfacePresenter#getView}
+     */
     @Override
     public final V getView() {
         return mView;
     }
 
+    /**
+     * {@link AIInterfacePresenter#getContext}
+     */
     @Override
     public final C getContext() {
         return mContext;
     }
 
+    /**
+     * {@link AIInterfacePresenter#isViewAttached}
+     */
     @Override
     public final boolean isViewAttached() {
+        return isViewAttached;
+    }
+
+    /**
+     * {@link AIInterfacePresenter#isViewCreated}
+     */
+    @Override
+    public final boolean isViewCreated() {
         return isViewCreated;
     }
 
+    /**
+     * {@link AIInterfacePresenter#hasContext}
+     */
     @Override
     public final boolean hasContext() {
         return mContext != null;
@@ -57,7 +87,8 @@ public abstract class AIPresenter<C, V> implements AIInterfacePresenter<C, V> {
     }
 
     /**
-     * Call this method from activity when view is created
+     * Call this method from activity when view is created.
+     * From now on you can call view methods
      */
     public void onViewCreated() {
         isViewCreated = true;
