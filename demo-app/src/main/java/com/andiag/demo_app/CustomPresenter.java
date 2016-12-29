180ff54234e8385222db5d9be39721fb537af27c
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-12-29 19:46:55 +0100

    Renaming things for version 0.2.1

---
+++ demo-app/src/main/java/com/andiag/demo_app/CustomPresenter.java
@@ -0,0 +1,56 @@
+package com.andiag.demo_app;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import com.andiag.core.presenters.AIPresenter;
+
+
+/**
+ * Created by Iago on 24/12/2016.
+ */
+public class CustomPresenter extends AIPresenter<Application, ActivityMain> {
+
+    /**
+     * Recommended singleton implementation for presenters
+     */
+    //region Singleton
+    private static CustomPresenter instance = null;
+
+    private CustomPresenter() {
+    }
+
+    public static CustomPresenter getInstance() {
+        if (instance == null) {
+            instance = new CustomPresenter();
+        }
+        return instance;
+    }
+    //endregion
+
+    /**
+     * Automatically called on presenter attach. VIEW MIGHT NOT BE CREATED JET
+     */
+    @Override
+    public void onViewAttached() {
+        Toast.makeText(getContext(), "Presenter On View Attached", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Call from activity when view loading ends
+     */
+    @Override
+    public void onViewCreated() {
+        super.onViewCreated();
+        callback();
+        /**
+         * Once this method occurs {@link AIPresenter.isViewAttached} will return True.
+         * Use it from your callbacks
+         */
+    }
+
+    private void callback() {
+        getView().presenterCallback("Activity callback");
+    }
+
+}
